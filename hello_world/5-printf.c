f29313e0dca5958cf6134ab7c074d7d6fe181e67
azooz <no-reply@example.com>
2025-04-24 14:46:18 +0300

    ADDED task 5 solution

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print the string "with proper grammar, but the outcome is a
+ * piece of art," and exit.
+ * Return: Always 0 (Succees)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
