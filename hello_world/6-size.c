f7998078c3f6b5588ca97d68dc004dc1b6d0e8f1
azooz <no-reply@example.com>
2025-04-24 15:00:05 +0300

    FIXED wrong output

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,13 +3,14 @@
 /**
  * main - Entry point
  *
- * Description: Display the sizes of various types on the computer it is compiled and run on.
+ * Description: Display the sizes of various types on the computer it is
+ * compiled and run on.
  * Return: Always 0 (Succees)
  */
 int main(void)
 {
 	printf("Size of a char: %li byte(s)\n", sizeof(char));
-	printf("Size of a int: %li byte(s)\n", sizeof(int));
+	printf("Size of an int: %li byte(s)\n", sizeof(int));
 	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %li byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %li byte(s)\n", sizeof(float));
