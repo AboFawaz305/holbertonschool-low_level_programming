3f56079f29ac1243b5e9c7909a267d5e7071183d
azooz <no-reply@example.com>
2025-04-24 18:40:34 +0300

    ADDED task 9 solution

---
+++ hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints "and that piece of art is useful\" - Dora Korpar, *
+ * 2015-10-19\n" into the standard error.
+ * Return: Always 0 (Succees)
+ */
+int main(void)
+{
+	perror("and that piece of art is useful "
+			"- Dora Korpar, * 2015-10-19\n"):
+	return (0);
+}
