0f8e8ac507fe48a3d2f90672a761b1feeadfc0f4
azooz <no-reply@example.com>
2025-04-24 14:37:20 +0300

    ADDED task 4 solution

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print the string "Programming is like building a multilingual puzzle" and exit.
+ * Return: Always 0 (Succees)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
