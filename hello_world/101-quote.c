f3762da0d56496daf151d73b40f19aa7903d6b61
azooz <no-reply@example.com>
2025-04-26 15:25:18 +0300

    FIXED wrong output

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -4,13 +4,13 @@
 /**
  * main - Entry point
  *
- * Description: prints "and that piece of art is useful\" - Dora Korpar, *
+ * Description: prints "and that piece of art is useful\" - Dora Korpar, 
  * 2015-10-19\n" to the standard error stream.
  * Return: Always 1 (Failure)
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful "
-			"- Dora Korpar, * 2015-10-19\n", 59);
+			"- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
