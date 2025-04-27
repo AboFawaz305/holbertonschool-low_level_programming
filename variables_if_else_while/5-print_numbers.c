ac328ef704cba4b2150c8605e9ab96bee5f9b9c0
azooz <no-reply@example.com>
2025-04-27 16:46:33 +0300

    ADDED project 2 task 6 solution

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet in lowercase and then print a newline.
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
