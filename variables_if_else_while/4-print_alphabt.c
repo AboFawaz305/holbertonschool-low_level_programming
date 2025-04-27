74ba44b44dd089caea999e80d06ba26064312ede
azooz <no-reply@example.com>
2025-04-27 16:44:13 +0300

    ADDED project 2 task 5 solution

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet in lowercase and then print a newline.
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
