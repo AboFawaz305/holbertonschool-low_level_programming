159157d12ebcbc40334fa21e5867b416c13dddce
azooz <no-reply@example.com>
2025-04-27 14:58:07 +0300

    Add project 2 task 1 solution

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: generate a random number and then print it as such
+ * {number} is {positive | negative}
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int n;
+	char* sign  = "negative";
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if ( n > 0 )
+	{
+		sign = "positive";
+	}
+	else if ( n == 0 )
+	{
+		sign = "zero";
+	}
+	printf("%d is %s\n", n, sign);
+	return (0);
+}
