35b74b1a1a6bfd0f6497b82f7a40a3e135129d8a
azooz <no-reply@example.com>
2025-04-28 16:17:13 +0300

    FIXED code style error

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,16 +3,16 @@
 /**
  * main - Entry point.
  *
- * Description: print a single character followed by a newline.
+ * Description: print a "_putchar" character followed by a newline.
  * Return: Always (0) Success
  */
 int main(void)
 {
 	int c;
-	/* Strings are NULL terminitated in C */ 
+	/* Strings are NULL terminitated in C */
 	char msg[] = "_putchar\0";
 
-	for(c = 0; msg[c] != '\0'; c++)
+	for (c = 0; msg[c] != '\0'; c++)
 	{
 		_putchar(msg[c]);
 	}
