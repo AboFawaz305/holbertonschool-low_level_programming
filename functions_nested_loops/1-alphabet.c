cd1ccb2c72e5ae81880b51dd0270d8388b067018
azooz <no-reply@example.com>
2025-04-28 16:26:11 +0300

    ADDED project 3 task 1 solution

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+int _putchar(char);
+
+/**
+ * print_alphabet - print the alphabet from a to z followed by a newline.
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	return;
+}
