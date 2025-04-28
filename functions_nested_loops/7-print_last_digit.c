bc2da3ca8525fcb81b6bca5666449cabbfd6d7e4
azooz <no-reply@example.com>
2025-04-28 20:34:30 +0300

    ADDED project 3 task 8 solution

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - return and print last digit of n
+ * @n: the number
+ *
+ * Return: the last digit of n
+ */
+int print_last_digit(int n)
+{
+	int an, ld;
+
+	an = _abs(n);
+	ld = an % 10;
+	_putchar(ld + '0');
+
+	return (ld);
+}
