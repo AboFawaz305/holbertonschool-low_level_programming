17f12f83793c3fb184682312907c913ecd1d37bb
azooz <no-reply@example.com>
2025-04-28 15:55:08 +0300

    ADDED project 3 task 13 solution

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "./_putchar.h"
+
+/**
+ * main - Entry point.
+ *
+ * Description: print a single character followed by a newline.
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	_putchar('c');
+	_putchar('\n');
+	return (0);
+}
