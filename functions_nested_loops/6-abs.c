3d3537f57014a2bf0c7fc0216566426108b982d6
azooz <no-reply@example.com>
2025-04-28 20:29:32 +0300

    ADDED project 3 task 7 solution

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute values of n
+ * @n: the number
+ *
+ * Return: the absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return -n;
+	return n
+}
