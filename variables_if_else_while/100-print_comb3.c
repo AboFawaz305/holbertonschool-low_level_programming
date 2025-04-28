b889bb9bfdc27199288cc16cb453d4f8bda079a7
azooz <no-reply@example.com>
2025-04-28 14:14:16 +0300

    ADDED project 2 task 11 solution

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet in lowercase and then print a newline.
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		int d;
+		for (d = c; d <= 9; d++)
+		{
+			if (d == c)
+				continue;
+			if (c != 0 && d != 1)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			putchar('0' + c);
+			putchar('0' + d);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
