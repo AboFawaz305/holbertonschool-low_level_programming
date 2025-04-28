e1eca8ecc81dc9331956489c979603efeab02a0a
azooz <no-reply@example.com>
2025-04-28 16:15:56 +0300

    FIXED wrong output

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,7 +8,14 @@
  */
 int main(void)
 {
-	_putchar('c');
+	int c;
+	/* Strings are NULL terminitated in C */ 
+	char msg[] = "_putchar\0";
+
+	for(c = 0; msg[c] != '\0'; c++)
+	{
+		_putchar(msg[c]);
+	}
 	_putchar('\n');
 	return (0);
 }
