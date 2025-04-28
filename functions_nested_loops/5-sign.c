1bc6b14da06ff64393a5140c17e34d137af76204
azooz <no-reply@example.com>
2025-04-28 20:06:05 +0300

    FIXED code style error

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,8 +2,9 @@
 
 /**
  * sign - checks the sign of a number and prints the sign
- *
  * @n: the number to be checked
+ *
+ * Description: checks the sign and print it to the standard output
  * Return: 1 if n is above 0 and returns 0 if n == 0 and -1 otherwise
  */
 int print_sign(int n)
