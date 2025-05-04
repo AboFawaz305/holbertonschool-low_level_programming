4631c274ee6f3a43d40e9fa10c5dcdd5bf4d2e42
azooz <no-reply@example.com>
2025-05-04 20:59:59 +0300

    FIXED wrong output

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,14 @@
 #include "main.h"
 #include <stdio.h>
+#include <inttypes.h>
+
+void print_fib(long int x, long int y)
+{
+	if (x != 0)
+		printf("%ld%-ld, ", x, y);
+	else
+		printf("%ld, ", y);
+}
 
 /**
  * main - print the first 98 fibonacci numbers starting from 1 and 2
@@ -8,20 +17,34 @@
  */
 int main(void)
 {
-	long double f1, f2, n, temp;
-	char* str;
+	long int fibc_0, fibc_1, fibn_0, fibn_1, tmp0, tmp1;
+	int n;
 
+	fibc_0 = 0;
+	fibc_1 = 1;
+	fibn_0 = 0;
+	fibn_1 = 2;
+	tmp0 = 0;
+	tmp1 = 0;
 	n = 0;
-	for (f1 = 1, f2 = 2; n < 97; n++)
+	for (; n < 97; n++)
 	{
-	temp = f2;
-
-		printf("%.0Lf, ", f1);
-		f2 = f2 + f1;
-		f1 = temp;
+		print_fib(fibc_0, fibc_1);
+		tmp0 = fibn_0;
+		tmp1 = fibn_1;
+		fibn_1 += fibc_1;
+		if (fibn_1 < 0)
+		{
+			fibn_1 = fibn_1 & 0x7FFFFFFFFFFFFFFF;
+			fibn_0 += 1;
+		}
+		fibn_0 += fibc_0;
+		fibc_0 = tmp0;
+		fibc_1 = tmp1;
 	}
-	printf("%.0Lf", f1);
+	printf("%ld%-ld", fibc_0, fibc_1);
 	putchar('\n');
 
 	return (0);
 }
+
