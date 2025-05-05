31bfb022e21d0f21a4ab29e1b32eef1df0c454bf
azooz <no-reply@example.com>
2025-05-05 16:40:05 +0300

    FIXED code style error

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 /**
  * mul - mutliply a and b
  * @a: the first multiplication operand
- * @a: the second multiplication operand
+ * @b: the second multiplication operand
  *
  * Return: the result of multiplying a with b
  */
 int mul(int a, int b)
 {
-	return (a*b);
+	return (a * b);
 }
