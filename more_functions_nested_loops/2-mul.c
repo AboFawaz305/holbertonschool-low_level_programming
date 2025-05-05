c8accb925c40a58cd0749ddb88199000a407867a
azooz <no-reply@example.com>
2025-05-05 16:38:53 +0300

    ADDED project 4 task 4 solution

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - mutliply a and b
+ * @a: the first multiplication operand
+ * @a: the second multiplication operand
+ *
+ * Return: the result of multiplying a with b
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+}
