83c13580092bd6ea0e3635e1e41468ade405e019
azooz <no-reply@example.com>
2025-05-05 16:33:07 +0300

    FIXED code style error

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
  * _isupper - returns 1 if c is uppercase and 0 otherwise
  * @c: the character to be checked
  *
- * Return: returns 1 if c is uppercase and 0 otherwise 
+ * Return: returns 1 if c is uppercase and 0 otherwise
  */
 int _isupper(int c)
 {
