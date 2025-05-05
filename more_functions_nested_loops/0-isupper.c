d3110dbb8af30cb2ba749e9c1d21871d3a5f33c6
azooz <no-reply@example.com>
2025-05-05 12:34:59 +0300

    ADDED project 4 task 1 solution

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+/**
+ * _isupper - returns 1 if c is uppercase and 0 otherwise
+ * @c: the character to be checked
+ *
+ * Return: returns 1 if c is uppercase and 0 otherwise 
+ */
+int _isupper(int c)
+{
+	return (c > 64 && c < 91);
+}
