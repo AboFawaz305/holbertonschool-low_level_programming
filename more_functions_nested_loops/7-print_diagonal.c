c77536b0b4d8a2cb313d3bd31855555a48109839
azooz <no-reply@example.com>
2025-05-06 14:39:08 +0300

    FIXED code style error

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -4,7 +4,7 @@
  * print_diagonal - print a diagonal of length n
  * @n: diagonal length and should
  *
- * Description: print a diagonal using '\' characters the diagonal length 
+ * Description: print a diagonal using '\' characters the diagonal length
  * should be at least 0. if n is less than or equal zero just prints an empty
  * line.
  */
