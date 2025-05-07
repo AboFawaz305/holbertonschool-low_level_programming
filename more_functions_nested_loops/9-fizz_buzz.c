d0d8ad26124d9b5cf5c6018fb7d92d561f6a5436
azooz <no-reply@example.com>
2025-05-07 12:03:11 +0300

    ADDED project 4 task 10 solution

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Fizz-Buzz test
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d", i);
+		printf(" ");
+	}
+	putchar('\n');
+	return EXIT_SUCCESS;
+}
