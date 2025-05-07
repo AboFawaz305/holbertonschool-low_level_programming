a273b27fab03f0669d04c43d0def67a2a65ff529
azooz <no-reply@example.com>
2025-05-07 12:20:09 +0300

    FIXED wrong output

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if (i % 3 == 0)
 			printf("Fizz");
@@ -18,5 +18,6 @@ int main(void)
 			printf("%d", i);
 		printf(" ");
 	}
+	printf("Buzz");
 	return EXIT_SUCCESS;
 }
