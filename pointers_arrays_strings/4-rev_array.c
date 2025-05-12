39b8538336ffe1d50e8b42994399f5cae4b7e3c5
azooz <no-reply@example.com>
2025-05-12 12:30:34 +0300

    ADDED project 6 task 4 solution

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+/**
+ * reverse_array - reverse the array a with size n
+ * @a: the array to be reversed
+ * @n: the size of a
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0, temp;
+
+	while (i < (n / 2))
+	{
+		temp = a[i];
+		a[i] = a[n - i -1];
+		a[n - i - 1] = temp;
+		i++;
+	}
+}
