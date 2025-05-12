05b4691c99cba225913c84dc6f9ebae21adfd42a
azooz <no-reply@example.com>
2025-05-12 12:49:44 +0300

    ADDED project 6 task 6 solution

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+/**
+ * cap_string - capitalize all words of a string
+ * @s: the string to capitalize
+ *
+ * Return: s
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - 32;
+
+	while (s[i])
+	{
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
+			s[i] == ',' || s[i] == ';' || s[i] == '.' ||
+			s[i] == '!' || s[i] == '?' || s[i] == '"' ||
+			s[i] == '(' || s[i] == ')' || s[i] == '{' ||
+			s[i] == '}')
+			if (s[i+1] >= 'a' && s[i+1] <= 'z')
+				s[i+1] = s[i+1] - 32;
+		i++;
+	}
+
+	return (s);
+}
