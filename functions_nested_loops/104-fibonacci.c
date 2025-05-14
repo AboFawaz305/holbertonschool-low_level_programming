4a2c8c4b79d55729bb608b7cd69d9414b0cc480c
azooz <no-reply@example.com>
2025-05-14 17:11:37 +0300

    ADDED project 3 task 16 solution

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,8 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-#include <inttypes.h>
+#include <stdint.h>
 
-void print_fib(long int x, long int y)
+/**
+ * print_fib - print a 128-bit number from its 2 64-bit parts
+ * @x: the leftmost part
+ * @y: the rightmost part
+ */
+void print_fib(int64_t x, int64_t y)
 {
 	if (x != 0)
 		printf("%ld%-ld, ", x, y);
@@ -17,7 +22,7 @@ void print_fib(long int x, long int y)
  */
 int main(void)
 {
-	long int fibc_0, fibc_1, fibn_0, fibn_1, tmp0, tmp1;
+	int64_t fibc_0, fibc_1, fibn_0, fibn_1, tmp0, tmp1;
 	int n;
 
 	fibc_0 = 0;
@@ -27,6 +32,7 @@ int main(void)
 	tmp0 = 0;
 	tmp1 = 0;
 	n = 0;
+
 	for (; n < 97; n++)
 	{
 		print_fib(fibc_0, fibc_1);
@@ -35,7 +41,7 @@ int main(void)
 		fibn_1 += fibc_1;
 		if (fibn_1 < 0)
 		{
-			fibn_1 = fibn_1 & 0x7FFFFFFFFFFFFFFF;
+			fibn_1 &= 0x7FFFFFFFFFFFFFFF;
 			fibn_0 += 1;
 		}
 		fibn_0 += fibc_0;
