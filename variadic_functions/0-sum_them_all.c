48a608a58601d2b195d74a3f17a9d6d06663e2a3
azooz <no-reply@example.com>
2025-05-28 18:02:10 +0300

    ADDED project 13 task 0 solution

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - return the sum for all the given arguments 
+ * @n: the number of numbers to sum
+ * @num*: a number to sum
+ *
+ * Return: the sum of all the arguments without n
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int unsigned i, acc;
+
+	if (n == 0)
+		return (0);
+
+	acc = 0;
+	i = 0;
+	va_start(ap, n);
+	while (i < n)
+	{
+		acc += va_arg(ap, int);
+		i++;
+	}
+	va_end(ap);
+	return (acc);
+}
